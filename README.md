c06bf01fe53838fbbe4d05ca803e20b33f58891f
Shay Punter <shay@shaypunter.co.uk>
2020-05-24 00:13:26 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # VanishAllPlayers
-VanishAllPlayers - A simple plugin that automatically vanishes all players. Download: [link]
+VanishAllPlayers is a simple, lightweight plugin that automatically makes all players invisible when they join.
+
+This plugin is designed to be used in Lobby / Hub servers.
+
+Download: [link]
+
+
+####Contributions
+If you would like to contribute to the plugin, please open a pull request. All code you use must be compatible with the GPL v3 license and will be published under the license.
---
+++ src/main/java/co/uk/shaypunter/vap/VanishAllPlayers.java
@@ -0,0 +1,21 @@
+package co.uk.shaypunter.vap;
+
+import co.uk.shaypunter.vap.events.onJoin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VanishAllPlayers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new onJoin(), this);
+    }
+
+    /**
+     * Get the instance of the VanishAllPlayers class
+     * @return VanishAllPlayers instance
+     */
+    public static VanishAllPlayers getInstance() {
+        return getPlugin(VanishAllPlayers.class);
+    }
+
+}
